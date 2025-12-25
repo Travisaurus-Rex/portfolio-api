850d4330b2aab1086be16ae419f7d76dc257ccc0
Travis Adams <brokenCD@live.com>
2025-12-25 17:37:51 +0700

    add geo debug route

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,15 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
+app.get("/debug/geo", (c) => {
+  return c.json({
+    country: c.req.header("cf-ipcountry"),
+    ip: c.req.header("cf-connecting-ip"),
+    colo: c.req.header("cf-ray")?.split("-")[1],
+    userAgent: c.req.header("user-agent"),
+  });
+});
+
 app.route("/events", events);
 
 export default app;
